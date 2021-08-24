928b2935e10a77a31591b17d1eae02dfab8810e2
speae <speae123456@daum.net>
2021-08-24 17:41:21 +0900

    jetson-gpio_test

---
+++ jetson_gpio_led_test.py
@@ -0,0 +1,13 @@
+import time
+
+import RPi.GPIO as GPIO
+
+led_pin = 29
+sec = 5
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(led_pin, GPIO.OUT)
+GPIO.output(led_pin, True)
+time.sleep(5)
+GPIO.output(led_pin, False)
+GPIO.cleanup(led_pin)
